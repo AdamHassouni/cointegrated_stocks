6ecebdf3f028e06d40a245206c1d566e215bc1ad
AdamHassouni <adamhassouni111@gmail.com>
2024-07-21 19:50:08 +0100

    commit 2

--- cojntegration_test.r
+++ cojntegration_test.r
@@ -1,28 +1,28 @@
 library("quantmod")
 library("tseries")
-## Obtain GC=F and UNG
-getSymbols("GM", from="2018-11-11", to="2023-01-01")
-getSymbols("F", from="2018-11-11", to="2023-01-01")
+## Obtain NVDA and ROBO
+getSymbols("NVDA", from="2022-01-01", to="2024-01-01")
+getSymbols("ROBO", from="2022-01-01", to="2024-01-01")
 
 ## Utilise the backwards-adjusted closing prices
-adAdj = unclass(Cl(`MSFT`))
-bAdj = unclass(Cl(AAPL))   
+adAdj = unclass(Cl(`NVDA`))
+bAdj = unclass(Cl(`ROBO`))   
 
-# Plotting GC=F
+# Plotting NVDA 
 plot(adAdj, type="l", xlim=c(0, length(adAdj)), ylim=range(c(adAdj, bAdj)),
      xlab="Index", ylab="Backward-Adjusted Prices in USD", col="blue")
 title(main="Backward-Adjusted Prices of GC=F and UNG")
 par(new=TRUE)
-# Plotting UNG on the same graph
+# Plotting ROBO on the same graph
 plot(bAdj, type="l", xlim=c(0, length(bAdj)), ylim=range(c(adAdj, bAdj)),
      axes=FALSE, xlab="", ylab="", col="red")
 axis(side=4)
 mtext("UNG Adjusted Price", side=4, line=3)
-legend("topright", legend=c("MSFT", "AAPL"), col=c("blue", "red"), lty=1)
+legend("topright", legend=c("NVDA", "ROBO"), col=c("blue", "red"), lty=1)
 
 ## Plot a scatter graph of the ETF adjusted prices
-plot(adAdj, bAdj, xlab="ARNC Backward-Adjusted Prices",
-     ylab="UNG Backward-Adjusted Prices")
+plot(adAdj, bAdj, xlab="NVDA Backward-Adjusted Prices",
+     ylab="ROBO Backward-Adjusted Prices")
 
 comb = lm(adAdj~bAdj)
 
